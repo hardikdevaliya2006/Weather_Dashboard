e831c687772611d1a67fd8285124588acd5f080d
hardikdevaliya2006 <hardikdevaliya2006@gmail.com>
2025-06-03 20:42:51 +0530

    feat: enhance WeatherForecastDashboard to display location data with improved layout

--- src/components/weather-data-components/WeatherForecastDashboard.jsx
+++ src/components/weather-data-components/WeatherForecastDashboard.jsx
@@ -1,10 +1,16 @@
-import { useParams } from "react-router"
+import { useSelector } from "react-redux";
+import { useParams } from "react-router";
 
 const WeatherForecastDashboard = () => {
-  const { location } = useParams()
+  const { location } = useParams();
+  const { locationRawData } = useSelector((state) => state.search);
   return (
-    <div>{location}</div>
-  )
-}
+    <div className="flex items-center justify-center flex-col gap-5">
+      <div className="location text-xl font-semibold">{location}</div>
 
-export default WeatherForecastDashboard
+      <div className="data">{JSON.stringify(locationRawData)}</div>
+    </div>
+  );
+};
+
+export default WeatherForecastDashboard;
