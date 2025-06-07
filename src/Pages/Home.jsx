35ab24b7cf66239d7ee5c55d350ceaaa803e886e
hardikdevaliya2006 <hardikdevaliya2006@gmail.com>
2025-06-07 21:50:57 +0530

    feat: implement Navbar, SearchBox, and Mode components with dynamic greeting

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,13 @@
 import WeatherForecastDashboard from "../components/weather-data-components/WeatherForecastDashboard";
-import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/Footer";
 import "../App.css";
+import Navbar from "../components/Toggle-Bar/Navbar";
 
 const Home = () => {
   return (
     <main className="mainContantWrap bg-base-light-primary dark:bg-base-dark-primary sm:h-screen w-full">
       <header>
-        {/* <Navbar /> */}
+        <Navbar />
       </header>
       <section>
         <WeatherForecastDashboard />
--- src/Pages/QureyPage.jsx
+++ src/Pages/QureyPage.jsx
@@ -1,13 +1,13 @@
 import WeatherForecastDashboard from "../components/weather-data-components/WeatherForecastDashboard";
-import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/Footer";
 import "../App.css";
+import Navbar from "../components/Toggle-Bar/Navbar";
 
 const QureyPage = () => {
   return (
     <main className="mainContantWrap bg-base-light-primary dark:bg-base-dark-primary h-screen w-full">
       <header>
-        {/* <Navbar /> */}
+        <Navbar />
       </header>
       <section>
         <WeatherForecastDashboard />
